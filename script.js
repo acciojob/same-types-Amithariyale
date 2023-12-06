2f369919e901f0fa9eb49854fcba1b0c07a2d257
Amit Hariyale <57282097+Amithariyale@users.noreply.github.com>
2023-12-07 00:39:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 function isSameType(value1, value2) {
   //your js code here
-	
-	return typeof value1==typeof value2;
+	return value1===value2
+
 	
 }
-
 
 // do not change the code below.
 let value1 = prompt("Enter Start of the Range.");
